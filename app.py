067a9536f9906a348dd460440426232fefd295b1
LittleSaltFish <saltfish@whut.edu.cn>
2021-04-21 21:14:22 +0800

    Version2-init

--- app.py
+++ app.py
@@ -8,19 +8,23 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/zone4")
+def re4():
+    return render_template("第四展区.html")
+
 @app.route("/zone3")
 def re3():
-    return render_template("introduction-第三展区.html")
+    return render_template("第三展区.html")
 
 
 @app.route("/zone2")
 def re2():
-    return render_template("introduction-武汉工业大学部分讲解.html")
+    return render_template("第二展区.html")
 
 
 @app.route("/zone1")
 def re1():
-    return render_template("introduction-理工大部分讲解稿.html")
+    return render_template("第一展区.html")
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=5001,debug=True)
--- tmp.py
+++
@@ -1,8 +0,0 @@
-num = 2021
-for i in range(1, 1000000):
-    for j in str(i) :
-        if j=="1":
-            num -= 1
-    if num == 0:
-        print(i)
-        break
