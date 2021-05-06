c064183f996ffe098891175a7c35cb7d1c78b672
LittleSaltFish <saltfish@whut.edu.cn>
2021-05-06 23:57:45 +0800

    点赞没做完

--- user/urls.py
+++ user/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path("login/", views.login),
-    path("register/", views.register),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
     path("logout/", views.logout, name="logout"),
-    path("comment/", views.make_comment, name="comment"),
-    path("charts/", views.admim_charts, name="admim_charts"),
-    path("tables/", views.admim_tables, name="admim_tables"),
+    path("MakeComment/", views.make_comment, name="make_comment"),
+    # path("charts/", views.admim_charts, name="admim_charts"),
+    path("comments/", views.admim_comments, name="admim_comments"),
     path("users/", views.admim_users, name="admim_users"),
+    path("LikeComment/", views.like_comment, name="like_comment"),
 ]
--- user/views.py
+++ user/views.py
@@ -12,12 +12,12 @@ def register(request):
     if request.method == "GET":
         return render(request, "register.html")
     if request.method == "POST":
-        message=None
+        message = None
         name = request.POST.get("name")
         password = request.POST.get("password")
         if user.objects.filter(username=name):
-            message="用户名已被使用，换一个吧"
-            return render(request, "register.html",{"message":message})
+            message = "用户名已被使用，换一个吧"
+            return render(request, "register.html", {"message": message})
         else:
             user.objects.create_user(username=name, password=password)
             return HttpResponseRedirect("/user/login/")
@@ -25,7 +25,7 @@ def register(request):
 
 def login(request):
     if request.method == "GET":
-        return render(request, "login.html",{"message":None})
+        return render(request, "login.html", {"message": None})
     if request.method == "POST":
         name = request.POST.get("name")
         password = request.POST.get("password")
@@ -35,8 +35,8 @@ def login(request):
             auth.login(request, user)
             return HttpResponseRedirect("/")
         else:
-            message="请检查用户名和密码"
-            return render(request, "login.html",{"message":message})
+            message = "请检查用户名和密码"
+            return render(request, "login.html", {"message": message})
 
 
 def logout(request):
@@ -45,17 +45,17 @@ def logout(request):
         return HttpResponseRedirect("/")
 
 
-def admim_charts(request):
-    if request.method == "GET" and request.user.is_authenticated:
-        return render(request, "Admin-Charts.html")
-    else:
-        return render(request, "Admin-404.html")
+# def admim_charts(request):
+#     if request.method == "GET" and request.user.is_authenticated:
+#         return render(request, "Admin-Charts.html")
+#     else:
+#         return render(request, "Admin-404.html")
 
 
-def admim_tables(request):
+def admim_comments(request):
     if request.method == "GET" and request.user.is_authenticated:
         comments = comment.objects.all()
-        return render(request,"Admin-Tables.html",{"comments": comments})
+        return render(request, "Admin-Comments.html", {"comments": comments})
     else:
         return render(request, "Admin-404.html")
 
@@ -63,18 +63,23 @@ def admim_tables(request):
 def admim_users(request):
     if request.method == "GET" and request.user.is_authenticated:
         users = user.objects.all()
-        return render(request,"Admin-Users.html",{"users": users})
+        return render(request, "Admin-Users.html", {"users": users})
     else:
         return render(request, "Admin-404.html")
 
+
 def make_comment(request):
     if request.method == "POST":
-        comment_content = request.POST["comment_content"].replace("[","<").replace("]",">")
+        comment_content = (
+            request.POST["comment_content"].replace("[", "<").replace("]", ">")
+        )
         user_name = request.POST["user_name"]
         user_id_str = request.POST["user_id"]
         user_id = user.objects.filter(id=user_id_str).first()
-        user_photo_url = user.objects.values("photo_url").get(id=user_id_str)["photo_url"]
-        
+        user_photo_url = user.objects.values("photo_url").get(id=user_id_str)[
+            "photo_url"
+        ]
+
         comment.objects.create(
             comment_content=comment_content,
             comment_hot_rate=0,
@@ -93,15 +98,25 @@ def list_comment(request):
     if request.method == "GET":
         comment_by_time = comment.objects.all().order_by("-create_time")[:20]
         comment_by_hot_rate = comment.objects.all().order_by("-comment_hot_rate")[:20]
-        
+
         # for i in range(len(comment_by_hot_rate)):
         #     id= comment_by_hot_rate[i].user_id_id
         #     print(id)
         #     if user.objects.filter(username=id).is_delete:
         #         comment_by_hot_rate[i].user_id=None
-        
+
         return render(
             request,
             "BBS.html",
             {"by_time": comment_by_time, "by_hot": comment_by_hot_rate},
         )
+
+def like_comment(request):
+    if request.method == "POST":
+        comment_id=request.POST["comment_id"]
+        comment = comment.objects.get(id=comment_id)
+        comment.comment_hot_rate+=1
+        comment.save()
+        return HttpResponseRedirect("/BBS")
+    else:
+        return render(request, "Front-404.html")
